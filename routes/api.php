85850052cabad968c9e747f51ac8e0fc0e47700c
Vikash Pathak <vikashh.pathak@gmail.com>
2020-01-31 19:16:25 +0530

    Add airlock middleware.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
+
+
+Route::middleware('auth:airlock')->get('/user', function (Request $request) {
     return $request->user();
 });
